a6090e2ec64ff7035e2f3b3b2922741c5c0c533b
belavirag <139229267+belavirag@users.noreply.github.com>
2023-11-01 10:59:22 +0100

    complete TaskRepository todos

--- src/main/java/se/lexicon/g46todoapi/repository/TaskRepository.java
+++ src/main/java/se/lexicon/g46todoapi/repository/TaskRepository.java
@@ -1,19 +1,35 @@
 package se.lexicon.g46todoapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g46todoapi.domain.entity.Task;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-  // todo: select tasks by title
-  // todo: select tasks by person id
-  // todo: select tasks by status
-  // todo: select tasks by date between start and end
-  // todo: select tasks by deadline
-  // todo: select all un-assigned tasks
-  // todo: select all un-finished tasks
+    List<Task> findByTitle(String title);
+    List<Task> findByPersonId(long id);
+
+    List<Task> findByDone(boolean done);
+
+    List<Task> findByDeadlineBetween(LocalDate start, LocalDate end);
+    List<Task> findByDeadline(LocalDate deadLine);
+    @Query("select s from Task s where s.person is null")
+    List<Task> findAllUnassigned();
+    @Query("select s from Task s where not s.done")
+    List<Task> findAllUnfinished();
+
+  // todo: select tasks by title - done
+  // todo: select tasks by person id - done
+  // todo: select tasks by status - done
+  // todo: select tasks by date between start and end - done
+  // todo: select tasks by deadline - done
+  // todo: select all un-assigned tasks - done
+  // todo: select all un-finished tasks - done
   // add more as needed...
 
 }
